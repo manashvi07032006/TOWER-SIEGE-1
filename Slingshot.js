0fa175b8e8e224f70b5cd3326a20f7cf78ca2af8
manashvi07032006 <80456520+manashvi07032006@users.noreply.github.com>
2021-04-07 15:31:26 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -11,20 +11,16 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-    /*attach(body){
-		this.sling.bodyA=body;
-	}*/
     fly(){
         this.sling.bodyA = null;
     }
 
     display(){
-		if(this.sling.bodyA){
-            var pointA = this.sling.bodyA.position;
-            var pointB = this.pointB;
-            //strokeWeight(10);
-			line(pointA.x, pointA.y, pointB.x, pointB.y);
-		}
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
     }
     
 }
